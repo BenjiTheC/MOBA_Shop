72576cce66e614ccdde4e10c5faeef787f1e342d
Benjamin Cai <benjamincaiyh@gmail.com>
2019-04-20 19:05:52 -0400

    slightly change

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 const userRoutes = require("./users-data-test");
+const mainRoutes = require("./main");
 const searchRoutes = require("./search");
 //const itemRoutes = require("./posts")
 
 const constructorMethod = app => {
+    app.use("/", mainRoutes);
     app.use("/users", userRoutes);
     app.use("/search", searchRoutes);
-    //app.use("/items", itemRoutes);
-    app.get('/', async (req, res) => {
-        res.render('template/home', { title: "MOBA Shop" })
-    })
 
     app.use("*", (req, res) => {
-        res.status(404).json({ error: "Not found" });
+        res.status(404).json({ error: "Page Not found" });
     });
 };
 
