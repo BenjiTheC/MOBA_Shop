ebb554aa5120db3e926ceb99330ae6c77add8b78
Siyuanhee <33169753+Siyuanhee@users.noreply.github.com>
2019-04-20 18:07:55 -0400

    Update users.js

--- data/users.js
+++ data/users.js
@@ -23,22 +23,31 @@ const exportedMethod = {
     //what information must be provided ?
     //structure of User:
     //userID: unique ID for registered users
+    //userName (string): The name set for user at account creation
+    //userPassword (string): The password set for privacy
     //userInfo: a sub-document with Information of users such as name, address, etc {name: xxx, address: xxx}
     //Email: email used for registration.
     //Virtual Concurrency: the “money” users will be using for trading on the platform
     //Purchase History: sub-document with record of purchasing of the user.
-    async addUser(userInfo, email) {
+    async addUser(userName, userPassword, userInfo, email) {
 
-        if (!userInfo.name) throw "you must provide a name"
-        if (!userInfo.address) throw "you must provide a address"
+        if (!userName) throw "you must provide a userName"
+        if (!userPassword) throw "you must provide a password"
+        if (!userInfo.nickName) throw "you must provide a nick name"
+        if (!userInfo.phone) throw "you must provide a phone number"
         if (!email) throw "you must provise a email"
 
+        if (!userInfo.age) userInfo.age =  null
+        if (!userInfo.hender) userInfo.gender = ""
+
         const userCollection = await users()
-        //do we need to store the password here
+
         let newUser = {
+            userName: userName,
+            userPassword: userPassword,
             userInfo: userInfo,
             email: email,
-            virtualConcurrency: 0.0,
+            virtualConcurrency: 0.0, // automatically assigned in the future
             purchaseHistory: []
         }
         const insertInfo = await userCollection.insertOne(newUser)
@@ -61,16 +70,18 @@ const exportedMethod = {
         if (!ObjectId.isValid(id)) throw "invalid input id"
 
         //add new info here
-        if (!updateUser.userInfo.name && !updateUser.userInfo.address && !updateUser.email){
-            throw "you must provide a name or address or email to be updated"
+        if (!updateUser.userInfo.nickName && !updateUser.userInfo.phone && !updateUser.userInfo.age && !updateUser.userInfo.gender && !updateUser.email){
+            throw "you must provide a nick name or phone or age or gender or email to be updated"
         }
         const userCollection = await users()
 
         let updateData = {}
 
         //add new info here
-        if (updateUser.userInfo.name) updateData.userInfo = updateUser.userInfo
-        if (updateUser.userInfo.address) updateData.userInfo = updateUser.userInfo
+        if (updateUser.userInfo.nickName || updateUser.userInfo.phone || updateUser.userInfo.age|| updateUser.userInfo.gender) {
+            updateData.userInfo = updateUser.userInfo
+        }
+
         if (updateUser.email) updateData.email = updateUser.email
 
         //use dot.flatten to flatten the data in updateData for update a specific data in userInfo
