a331847bbbbcbf397743520c94fe72309ea1f7c7
Benjamin Cai <benjamincaiyh@gmail.com>
2019-04-26 22:52:11 -0400

    Layout tempalte footer added

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const MOCK_USER_BENJI = {
+  userPic: "https://via.placeholder.com/512x512.png?text=User+Picture",
+  userName: "Benji",
+  userAsset: 65536,
+}
+
 function itemGenerator(repeat_time) {
   const itemSeed = {
       itemPic: "https://via.placeholder.com/300x300.png?text=Item+Picture",
@@ -22,7 +28,7 @@ function itemGenerator(repeat_time) {
 
 router.get('/', async (req, res) => {
   const itemList = itemGenerator(11);
-  return res.render('template/home', { title: "MOBA Shop", itemList: itemList });
+  return res.render('template/home', { title: "MOBA Shop", itemList: itemList, isLoggedIn: false, userInfo: MOCK_USER_BENJI });
 })
 
 module.exports = router;
