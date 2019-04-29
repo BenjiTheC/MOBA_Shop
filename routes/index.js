6b5f9578053773d342a2a3edaf0d8914b8de8c33
Benjamin Cai <benjamincaiyh@gmail.com>
2019-04-29 17:43:11 -0400

    Log in form, temp route for front end dev

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const userRoutes = require("./users");
 const mainRoutes = require("./main");
 const searchRoutes = require("./search");
-const itemRoutes = require("./items")
-const authenRoutes = require("./authen")
+const itemRoutes = require("./items");
+const authenRoutes = require("./authen");
+
+const testRoutes = require("./frontend_test");
 
 const constructorMethod = app => {
     app.use("/", mainRoutes);
@@ -10,6 +12,8 @@ const constructorMethod = app => {
     app.use("/items", itemRoutes);
     app.use("/search", searchRoutes);
     app.use("/authen", authenRoutes);
+
+    app.use("/test", testRoutes);
 
     app.use("*", (req, res) => {
         res.status(404).json({ error: "Page Not found" });
